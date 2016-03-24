eee9ccf9983bb0f0fbde56fce3ea599767229376
Huang BingDong <juzhong@users.noreply.github.com>
2016-03-24 13:08:38 +0800

    Update MD5.hpp

--- MD5.hpp
+++ MD5.hpp
@@ -6,12 +6,17 @@
 //  Copyright © 2016年 HUANG BINGDONG. All rights reserved.
 //
 
+//Warning: this class calculate md5 hash on some platform is error!If you import this class to your project,please test the hash
+//         whether or not is right which calculate by this class.          
+
 #ifndef MD5_hpp
 #define MD5_hpp
 
 #include <stdio.h>
 #include <string>
 
+//following macro is using for cross platform,but it has a bug,lead to on some platform calculate md5 hash is wrong!
+//such as Mac OS X Simulator for iPhone 5(this is not arm64 platform)
 # if defined(__LP32__)
 #  define LONG unsigned long
 #  define LONG64 unsigned long long
